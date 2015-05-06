4447001e2112290263be542fade5e1c587e08d2b
Leo Chiang <zola0419@kiss99.com>
2015-05-07 00:36:32 +0800

    helloWorldDemo

---
+++ helloWorldDemo/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  helloWorldDemo
+//
+//  Created by Leo on 2015/5/6.
+//  Copyright (c) 2015年 Leoswiftproject. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBOutlet var labelDisplay: UILabel!
+    
+    var counter = 0
+    
+    
+    @IBAction func buttonPressed(sender: AnyObject) {
+        labelDisplay.text = "HelloWorld \(counter++)"
+        
+    }
+
+}
+
